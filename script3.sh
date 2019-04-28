52fa3e208b1c8eac5076962a61733d8de8995bfc
matt712 <mzgadd@gmail.com>
2019-04-28 11:40:27 +0000

    frontend deployed, need to fix wildfly CORS

--- script3.sh
+++ script3.sh
@@ -5,7 +5,9 @@ cd src
 sudo sed -i 's/replaceme/NEW_URL/g' Constants.js 
 sudo sed -i 's/8080/8081/g' Constants.js
 cd ..
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm@latest -g
-npm install 
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+docker build . -t frontendapp
+docker run -d -p 80:80 --rm frontendapp
 
